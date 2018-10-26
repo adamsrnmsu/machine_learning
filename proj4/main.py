52bd5363852e8efcb43a963f5a6d924891e33c33
Ryan Adams <adamsrnmsu@gmail.com>
2018-10-26 05:32:44 -0600

    initial commit of proj4

---
+++ proj4/main.py
@@ -0,0 +1,42 @@
+'''
+:Name: Ryan C. Adams
+:Course: CS487 Applied Machine Learning I
+:Project 4: Regression
+
+Usage of the following sklearn classifers on 
+
+:Datasets: 
+    i. Housing dataset:
+       'housing.data.txt' from:
+       Canvas course files -> data
+
+    ii. California Revewable PRoduction 2010-2018 dataset:
+        'all_breakdown.csv' from:
+        https://www.kaggle.com/cheedcheed/california-wind-power-generation-forecasting/data
+
+
+:Classifiers: 
+    i. LinearRegression
+    ii. RANSACRegressor
+    iii. Ridge
+    iv. Lasso
+    v. normal equation
+    vi. one approach using non-linear regression
+
+'''
+
+import pandas as pd
+import numpy as np
+import pprint
+import matplotlib.pyplot as plt
+import datetime
+import argparse
+import os
+import sys
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
